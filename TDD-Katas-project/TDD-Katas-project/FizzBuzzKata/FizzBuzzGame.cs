def8a0eb9407d2c9fa6109bad49b042371066421
Krzysztof Sadowski <krzysztof.sadowski@totaljobsgroup.com>
2013-10-11 15:50:48 +0200

    fizz buzz impl

---
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/FizzBuzzGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace TDD_Katas_project.FizzBuzzKata
+{
+    public class FizzBuzzGame
+    {
+        internal string CheckNuber(int p)
+        {
+            string result = null;
+            
+            if (p%3 == 0)
+                result += "Fizz";
+
+            if (p%5 == 0)
+                result += "Buzz";
+
+            return result ?? p.ToString();
+        }
+    }
+}
--- TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/TestFizzBuzz.cs
+++ TDD-Katas-project/TDD-Katas-project/FizzBuzzKata/TestFizzBuzz.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace TDD_Katas_project.FizzBuzzKata
@@ -6,7 +8,75 @@ namespace TDD_Katas_project.FizzBuzzKata
     [TestFixture]
     [Category("TheFizzBuzzKata")]
     public class TestFizzBuzz
-    {        
+    {
+        [Test]
+        [TestCaseSource("MuliplesOfThreeOnly")]
+        public void ShouldPrintFizzIfNubmerCanBeDivideByThree(int mulipiOfThree)
+        {
+            var fizzBuzzGame = new FizzBuzzGame();
+
+            var output = fizzBuzzGame.CheckNuber(mulipiOfThree);
+
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        [TestCaseSource("MulipliesOfFiveOnly")]
+        public void ShouldPrintBuzzIfNubmerCanBeDivideByFive(int mulipiOfFive)
+        {
+            var fizzBuzzGame = new FizzBuzzGame();
+
+            var output = fizzBuzzGame.CheckNuber(mulipiOfFive);
+
+            Assert.AreEqual("Buzz", output);
+        }
+
+        [Test]
+        [TestCaseSource("MuliplesOfThreeAndFive")]
+        public void ShouldPrintBuzzIfNubmerCanBeDivideByFiveAndThree(int muliplieOfThreeAndFive)
+        {
+            var fizzBuzzGame = new FizzBuzzGame();
+
+            var output = fizzBuzzGame.CheckNuber(muliplieOfThreeAndFive);
+
+            Assert.AreEqual("FizzBuzz", output);
+        }
+
+        [Test]
+        [TestCaseSource("SomeOtherNumbers")]
+        public void ShouldPrintNumberAsString(int someOrdinaryNumber)
+        {
+            var fizzBuzzGame = new FizzBuzzGame();
+
+            var output = fizzBuzzGame.CheckNuber(someOrdinaryNumber);
+
+            Assert.AreEqual(someOrdinaryNumber.ToString(), output);
+        }
+        
+        public IEnumerable<int> MuliplesOfThreeOnly
+        {
+
+            get { return Enumerable.Range(1, 100).Where(x => x % 3 == 0 && x % 5 > 0); }
+
+        }
+
+        public IEnumerable<int> MulipliesOfFiveOnly
+        {
+
+            get { return Enumerable.Range(1, 100).Where(x => x % 5 == 0 && x % 3 > 0); }
+
+        }
+        public IEnumerable<int> MuliplesOfThreeAndFive
+        {
+
+            get { return Enumerable.Range(1, 100).Where(x => x % 5 == 0 && x % 3 == 0); }
+        }
+
+        public IEnumerable<int> SomeOtherNumbers
+        {
+
+            get { return Enumerable.Range(1, 100).Where(x => x % 5 > 0 && x % 3 > 0); }
+        }
     }
 }
 
